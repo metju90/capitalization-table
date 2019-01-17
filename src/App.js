13fb5bb9167cad9f8aa099241eccfe5a52e3a54a
Matthew <barbaramatthew90@gmail.com>
2019-01-18 00:57:35 +0100

    Checking for exceeding cap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import { Col, Row, Container } from "styled-bootstrap-grid";
+import { Investor, SmallText } from "./style";
 
 const investorsCommonVariables = {
-  cap: null,
+  cap: 2,
   multiplier: 1,
-  participating: false,
+  participating: true,
   payout: {
     liquidationPreference: null,
     paricipation: null
@@ -49,37 +50,61 @@ const App = () => {
         let balanceFromExit;
 
         // Giving the venture investor their inital investment
-        Object.keys(shareholders).reduce((balance, key) => {
-          if (key === "founders") return balance;
-          console.log("money left...", balance);
-          balance = balance - shareholders[key].invested;
-          // shareholders[key].payout =
-          //   exitValue * (shareholders[key].shares / 100);
-          shareholders[key].payout = {
-            liquidationPreference:
-              shareholders[key].invested * shareholders[key].multiplier
-          };
-          balanceFromExit = balance;
-          return balance;
-        }, exitValue);
-
+        Object.keys(shareholders)
+          .reverse() // To start from the latest Series
+          .reduce((balance, key) => {
+            if (key === "founders") return balance;
+            // console.log("money left...", key, balance);
+            balance = balance - shareholders[key].invested;
+            // shareholders[key].payout =
+            //   exitValue * (shareholders[key].shares / 100);
+            // const
+            shareholders[key].payout = {
+              liquidationPreference:
+                shareholders[key].invested * shareholders[key].multiplier
+            };
+            balanceFromExit = balance;
+            return balance;
+          }, exitValue);
+        console.log(
+          ">>>>>>  balance left after investors take init invest",
+          balanceFromExit
+        );
         // Sharing the remaing balance between all shareholders
         // (assuming all share holders are participants without cap)
         if (balanceFromExit) {
+          const investorsWithExceededCap = [];
           Object.keys(shareholders).reduce((balance, key) => {
             const {
               payout: { paricipation, liquidationPreference },
-              shares
+              shares,
+              invested,
+              cap
             } = shareholders[key];
+
+            const doesExceedCap =
+              liquidationPreference + balance * (shares / 100) > invested * cap;
+            if (doesExceedCap) {
+              investorsWithExceededCap.push(shareholders[key]);
+              console.log("!!!doesExceedCap ", key, doesExceedCap);
+              console.log(
+                "the difference is... ",
+                liquidationPreference +
+                  balance * (shares / 100) -
+                  invested * cap
+              );
+              balance +=
+                liquidationPreference +
+                balance * (shares / 100) -
+                invested * cap;
+            }
             shareholders[key].payout = {
               liquidationPreference,
-              paricipation: balance * (shares / 100)
+              paricipation: doesExceedCap
+                ? invested * cap
+                : balance * (shares / 100),
+              isCapReached: doesExceedCap
             };
-            console.log(
-              "the final balance is.. ",
-              key,
-              balance * (shares / 100)
-            );
             return balance;
           }, balanceFromExit);
         }
@@ -91,22 +116,28 @@ const App = () => {
 
   return (
     <Container>
+      <h2>Seniority Structure: Standard</h2>
       <Row>
         <Col md={5}>
           {Object.keys(shareholders).map(key => {
             const {
               title,
               shares,
-              payout: { liquidationPreference, paricipation }
+              payout: { liquidationPreference, paricipation, isCapReached }
             } = shareholders[key];
             return (
-              <div>
+              <Investor>
                 {`${title}: ${shares}%`}
                 {liquidationPreference && (
-                  <span>Payout LP: {liquidationPreference} </span>
+                  <div>Payout LP: {liquidationPreference} </div>
                 )}
-                {paricipation && <span>Payout Par: {paricipation} </span>}
-              </div>
+                {paricipation && (
+                  <div>
+                    Payout Par: {paricipation}{" "}
+                    {isCapReached && <SmallText>(Capped)</SmallText>}{" "}
+                  </div>
+                )}
+              </Investor>
             );
           })}
         </Col>
